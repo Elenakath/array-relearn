23a9722f1b450f3fddf82fac53f0512720f91757
elena_e <elenaka123@gmail.com>
2024-04-04 11:11:56 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var timeOfDay = [6, "noon", 8, "morning", "evening", 12];
+var meals = [];
+
+timeOfDay.splice(4, 0, "twilight", 2, 9);
+console.log(timeOfDay);
+
+var employeeAges = [15, 36, 78, 225, 17, 42, 19, 18];
+var age = employeeAges.indexOf(45);
+console.log(age);
